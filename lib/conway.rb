8dcd2e30161bc79e4a4a9355623458e1bd7820bd
derekbmcintire <derekbmcintire@gmail.com>
2017-11-17 17:05:13 -0500

    Get neighbors
    Get neighbors, including ones that don't exist

--- lib/conway.rb
+++ lib/conway.rb
@@ -2,22 +2,18 @@
  #hi
  class Board
    attr_accessor :board, :neighbors
-
    def initialize(size)
      @board = Array.new(size, Array.new(size, false))
      @neighbors = []
    end
-
    # cell is an array like [1,0]
    def get_neighbors(cell)
-     @board.length.times do |i|
-       current_cell = [i]
-       @board[i].length.times do |j|
-         current_cell.push(j)
-         @neighbors.push(current_cell)
-       end
-     end
+     a = cell[0]
+     b = cell[1]
+     @neighbors = [
+       [(a+1),b], [(a+1),(b+1)], [(a+1), (b-1)], [(a-1), b], [(a-1), (b+1)],
+       [(a-1), (b-1)], [a, (b-1)], [a, (b+1)]
+     ]
      @neighbors
    end
-
  end
