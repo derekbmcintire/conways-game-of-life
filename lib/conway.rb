b8ca9d1ec15f743d07129fb840c80bd94eee4082
derekbmcintire <derekbmcintire@gmail.com>
2017-11-17 15:42:07 -0500

    Set up get_neighbors method

--- lib/conway.rb
+++ lib/conway.rb
@@ -1,10 +1,14 @@
 # frozen_string_literal: true
  #hi
  class Board
-   attr_accessor :board
+   attr_accessor :board, :neighbors
 
    def initialize(size)
      @board = Array.new(size, Array.new(size, false))
+   end
+
+   def get_neighbors(cell)
+     @neighbors = []
    end
 
  end
